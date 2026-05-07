d2f543de43ebe715e3d55c58f9447ede6200a2c7
samuelelium <samuel.umoh000@gmail.com>
2026-05-07 19:39:12 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Tengo algo para ti mi carinño'.split('')
+const text = 'Für dich'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
